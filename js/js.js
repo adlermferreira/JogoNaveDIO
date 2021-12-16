94211df9623bcd001c1e0aab93e1d47f7b7cf1fb
Adler Melgaço Ferreira <adlermf07@gmail.com>
2021-12-16 17:18:44 -0300

    hp bar

--- js/js.js
+++ js/js.js
@@ -7,6 +7,7 @@ function start() { // Inicio da função start()
 	$("#fundoGame").append("<div id='inimigo2'></div>");
 	$("#fundoGame").append("<div id='amigo' class='anima3'></div>");
 	$("#fundoGame").append("<div id='placar'></div>");
+	$("#fundoGame").append("<div id='energia'></div>");
 
 	var TECLA = {
 		W: 87,
@@ -21,6 +22,7 @@ function start() { // Inicio da função start()
 	var pontos=0;
 	var salvos=0;
 	var perdidos=0;
+	var energiaAtual=3;
 
 	var jogo = {}
 	jogo.pressionou = [];
@@ -44,6 +46,7 @@ function start() { // Inicio da função start()
 		moveamigo();
 		colisao();
 		placar();
+		energia();
 	} 
 
 	function movefundo() {
@@ -148,7 +151,8 @@ function start() { // Inicio da função start()
 		var colisao5 = ($("#jogador").collision($("#amigo")));
 		var colisao6 = ($("#inimigo2").collision($("#amigo")));
 		
-		if (colisao1.length>0) {	
+		if (colisao1.length>0) {
+			energiaAtual--;
 			inimigo1X = parseInt($("#inimigo1").css("left"));
 			inimigo1Y = parseInt($("#inimigo1").css("top"));
 			explosao1(inimigo1X,inimigo1Y);
@@ -159,6 +163,7 @@ function start() { // Inicio da função start()
 		}
 
 		if (colisao2.length>0) {
+			energiaAtual--;
 			inimigo2X = parseInt($("#inimigo2").css("left"));
 			inimigo2Y = parseInt($("#inimigo2").css("top"));
 			explosao2(inimigo2X,inimigo2Y);
@@ -282,5 +287,25 @@ function start() { // Inicio da função start()
 
 	function placar() {
 		$("#placar").html("<h2> Pontos: " + pontos + " Salvos: " + salvos + " Perdidos: " + perdidos + "</h2>");
+	}
+
+	function energia() {
+	
+		if (energiaAtual==3) {
+			$("#energia").css("background-image", "url(imgs/energia3.png)");
+		}
+	
+		if (energiaAtual==2) {
+			$("#energia").css("background-image", "url(imgs/energia2.png)");
+		}
+	
+		if (energiaAtual==1) {
+			$("#energia").css("background-image", "url(imgs/energia1.png)");
+		}
+	
+		if (energiaAtual==0) {
+			$("#energia").css("background-image", "url(imgs/energia0.png)");
+		}
+	
 	}
 }
